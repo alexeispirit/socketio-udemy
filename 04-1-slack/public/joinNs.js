ef7f0884a286410ef2e460df3f4270b0fd842c40
alexeispirit <alexeispirit@gmail.com>
2019-08-21 12:04:32 +0500

    26-steps-7-9

--- 04-1-slack/public/joinNs.js
+++ 04-1-slack/public/joinNs.js
@@ -1,5 +1,5 @@
 function joinNs(endpoint) {
-  const nsSocket = io(`http://localhost:9000${endpoint}`);
+  nsSocket = io(`http://localhost:9000${endpoint}`);
   nsSocket.on("nsRoomLoad", nsRooms => {
     let roomList = document.querySelector(".room-list");
     roomList.innerHTML = "";
@@ -17,15 +17,38 @@ function joinNs(endpoint) {
         console.log(e.target.innerText);
       });
     });
+
+    // add room automatically... first time here
+    const topRoom = document.querySelector(".room");
+    const topRoomName = topRoom.innerText;
+    joinRoom(topRoomName);
   });
 
   nsSocket.on("messageToClients", msg => {
-    document.querySelector("#messages").innerHTML += `<li>${smg.text}</li>`;
+    document.querySelector("#messages").innerHTML += buildHTML(msg);
   });
 
   document.querySelector(".message-form").addEventListener("submit", e => {
     e.preventDefault();
-    const newMessage = document.querySelector("#user-message").nodeValue;
+    const newMessage = document.querySelector("#user-message").value;
     nsSocket.emit("newMessageToServer", { text: newMessage });
   });
+}
+
+function buildHTML(msg) {
+  const convertedDate = new Date(msg.time).toLocaleString();
+  const newHTML = `
+  <li>
+    <div class="user-image">
+      <img src="${msg.avatar}" />
+    </div>
+    <div class="user-message">
+      <div class="user-name-time">${
+        msg.username
+      } <span>${convertedDate}</span></div>
+      <div class="message-text">${msg.text}</div>
+    </div>
+  </li>
+  `;
+  return newHTML;
 }
--- 04-1-slack/slack.js
+++ 04-1-slack/slack.js
@@ -33,5 +33,31 @@ namespaces.forEach(namespace => {
     // a socket has connected to one of our chatgroup namespaces
     // send that ns group info back
     nsSocket.emit("nsRoomLoad", namespaces[0].rooms);
+    nsSocket.on("joinRoom", (roomToJoin, numberOfUsersCallback) => {
+      nsSocket.join(roomToJoin);
+      io.of("/wiki")
+        .in(roomToJoin)
+        .clients((err, clients) => {
+          numberOfUsersCallback(clients.length);
+        });
+    });
+
+    nsSocket.on("newMessageToServer", msg => {
+      const fullMsg = {
+        text: msg.text,
+        time: Date.now(),
+        username: "Alex",
+        avatar: "https://via.placeholder.com/30"
+      };
+      // Send this message to all the sockets that are in the room that this socket is in
+      // how to find out what rooms this socket is in?
+      // the user will be in the 2nd room in the object list (nsSocket.rooms)
+      // this is because the socket always joins its own room on connections
+      // get the keys
+      const roomTitle = Object.keys(nsSocket.rooms)[1];
+      io.of("/wiki")
+        .to(roomTitle)
+        .emit("messageToClients", fullMsg);
+    });
   });
 });
